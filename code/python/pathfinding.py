60feb6b08cc7c85edfede559af1271fbb1133ad1
Andrew Rooney <arrooney@ualberta.ca>
2018-11-18 17:06:15 -0700

    first

---
+++ code/python/pathfinding.py
@@ -0,0 +1,15 @@
+class Node:
+	def __init__(self, G, H, F):
+	self.G = G
+	self.H = H
+	self.F = F
+
+def main():
+	startNode = input().split()
+	gridSize = input().split()
+	for i in range(gridSize[0]):
+		
+		
+
+if __name__ == "__main__":
+	main()
