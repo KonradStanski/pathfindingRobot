5a42fc7c8f71031a3aac7161b2dcd247ac06f237
Andrew Rooney <arrooney@ualberta.ca>
2018-11-18 17:09:03 -0700

    Node class added

--- code/python/pathfinding.py
+++ code/python/pathfinding.py
@@ -1,15 +1,22 @@
 class Node:
-	def __init__(self, G, H, F):
-	self.G = G
-	self.H = H
-	self.F = F
+	def _init__(self, G, H, F, X, Y):
+		self.G = G
+		self.H = H
+		self.F = F
+		self.X = X
+		self.Y = Y
+	def setF(self):
+		self.F = self.H + self.G
 
 def main():
-	startNode = input().split()
-	gridSize = input().split()
-	for i in range(gridSize[0]):
-		
-		
+	print('we here')
+	gridSize = input().split() # format: X, Y
+	nodes = {}
+	for i in range(X):
+		for j in range(Y):
+			nodes[(i, j)] = Node(0, 0, 0, i, j)
+	print(nodes)
 
-if __name__ == "__main__":
+if __name__ == "main":
 	main()
+
