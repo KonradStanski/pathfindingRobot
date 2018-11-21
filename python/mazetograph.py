eb542357582ecef17747248bb78c422a224c66e0
konradStanski <kstanisz@ualberta.ca>
2018-11-20 23:07:38 -0700

    Auto Detect Start and End

--- python/mazetograph.py
+++ python/mazetograph.py
@@ -2,21 +2,44 @@ from PIL import Image
 import numpy as np
 
 #Opens the Image
-im_file = Image.open('mazes/maze1_11X11.gif')
+im = Image.open('mazes/maze1_11X11.gif')
 
-#Reads the image pixel information
-bin_maze = np.array(im_file)
+#Reads the image pixel information INDEXING IS ****(Y,X)****
+maze_arr = np.array(im)
 
 #Sets the width, height and maze size variables
-width = im_file.size[0]
-height = im_file.size[1]
+width = im.size[0]
+height = im.size[1]
 size = width * height
+
+#find start and end assuming start on top and end on botom
+# find first start
+for i in range(width):
+    pixel = im.getpixel((i,0))
+    if pixel == 0:
+        print('start coord:', i, 0)
+        maze_arr[0][i] = 2
+        break
+
+# find first end
+for i in range(width):
+    pixel = im.getpixel((i,height-1))
+    if pixel == 0:
+        print('end coord:', i, height-1)
+        maze_arr[height-1][i] = 3
+        break
+
+
+
+
+
+
 
 #Prints maze information for debugging
 print ('Maze width:', width)
 print ('Maze height:', height)
 print ('Maze size:', size, '\n')
-print (bin_maze)
+print (maze_arr)
 
 # create nearest neighbour pixel structure
 # catch edge cases *litteraly*
