4387c8b054e19be46fdc467cdf4320a86f5a2cce
konradStanski <kstanisz@ualberta.ca>
2018-11-20 22:26:48 -0700

    added mazes

--- python/mazetograph.py
+++ python/mazetograph.py
@@ -2,19 +2,23 @@ from PIL import Image
 import numpy as np
 
 #Opens the Image
-im_file = Image.open('mazes/maze1.png')
+im_file = Image.open('mazes/maze1_11X11.gif')
+
+#Reads the image pixel information
+bin_maze = np.array(im_file)
 
 #Sets the width, height and maze size variables
 width = im_file.size[0]
 height = im_file.size[1]
 size = width * height
 
-#Reads the image pixel information
-im_arr = np.array(im_file)
-
 #Prints maze information for debugging
 print ('Maze width:', width)
 print ('Maze height:', height)
 print ('Maze size:', size, '\n')
-print (im_arr)
+print (bin_maze)
+
+# create nearest neighbour pixel structure
+# catch edge cases *litteraly*
+
 
