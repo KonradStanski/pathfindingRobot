7f5c0e06683a1d9a532ca05430a3ed1c46116e76
konradStanski <kstanisz@ualberta.ca>
2018-11-22 16:17:16 -0700

    added animation

--- python/mazetograph.py
+++ python/mazetograph.py
@@ -1,11 +1,13 @@
 from PIL import Image
 import numpy as np
+from time import sleep
 
 #Opens the Image
 im = Image.open('mazes/maze1_11X11.gif')
 
 #Reads the image pixel information INDEXING IS ****(Y,X)****
 maze_arr = np.array(im)
+
 
 #Sets the width, height and maze size variables
 width = im.size[0]
@@ -17,8 +19,7 @@ size = width * height
 for i in range(width):
     pixel = im.getpixel((i,0))
     if pixel == 0:
-        start = [i, 0]
-        #maze_arr[0][i] = 4
+        start = [0, i]
         break
 
 # find first end
@@ -33,20 +34,23 @@ for i in range(width):
 ##########################################################
 # always turn  finding algorithm / just for fun?
 grid = maze_arr
+path = []
 def search(x, y):
     print('entering maze')
     if grid[x][y] == 2:
+        path.append([y, x])
         print ('found at %d,%d' % (x, y), '\n')
         return True
     elif grid[x][y] == 1:
         print ('wall at %d,%d' % (x, y))
         return False
     elif grid[x][y] == 3:
+        path.append([y, x])
         print ('visited at %d,%d' % (x, y))
         return False
 
     print ('visiting %d,%d' % (x, y))
-
+    path.append([y, x])
     # mark as visited
     grid[x][y] = 3
 
@@ -58,10 +62,24 @@ def search(x, y):
         return True
 
     return False
-search(start[1], start[0])
+search(start[0], start[1])
 ############################################################
 
+moves = []
+for i in range(len(path)-1):
+    xdist = path[i+1][0] - path[i][0]
+    ydist = path[i+1][1] - path[i][1]
+    print("i is:", i, "xdist: ", xdist, "ydist: ", ydist)
 
+
+#small animation
+maze_arr = np.array(im)
+for i in range(len(path)-1):
+    x = path[i][0]
+    y = path[i][1]
+    maze_arr[x][y] = 4
+    print(maze_arr)
+    sleep(0.1)
 
 
 #Prints maze information for debugging
@@ -70,10 +88,11 @@ print ('Maze height:', height)
 print ('Maze size:', size, '\n')
 print ('Maze start:' , start)
 print ('Maze end:', end, '\n')
-print (maze_arr, '\n')
-maze_arr = np.array(im)
-print (maze_arr)
-
+#print (maze_arr, '\n')
+#maze_arr = np.array(im)
+#print (maze_arr)
+print (path)
+print( moves )
 
 # create nearest neighbour pixel structure
 # catch edge cases *litteraly*
