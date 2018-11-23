059f299b1370ff83b244e8f8d30de77e9a897759
cmput274 <cmput274@ualberta.ca>
2018-11-22 23:35:17 -0700

    new node class, beginnings of some algorithm

--- python/maze.py
+++ python/maze.py
@@ -15,5 +15,27 @@ class node:
 		self.UN = (self.row+1, self.col)
 		self.DN = (self.row-1, self.col)
 
-me = node(1, 0, 4, 5, 1)
-print(me.RN)
+
+def compileNodes(height, width, maze, start, end):
+	nodes = {}
+	for i in range(width): # all columns
+		for j in range(height): # all rows
+			if mazeArr[j][i]:
+				if (j, i) != start and (j, i) != end:
+					nodes[(j, i)] = node(0, j, i, 0)
+				elif (j, i) == start:
+
+
+
+def main():
+
+    im = Image.open("/home/cmput274/ARCMPUT/arduino/final/274final/python/mazes/maze1_11X11.gif")
+    mazeArr = np.array(im)
+    width = im.size[0]
+    height = im.size[1]
+    start = [(0, i) for i in range(width) if im.getpixel((i, 0)) == 0]
+    end = [(height-1, i) for i in range(width) if im.getpixel((i, height-1)) == 0]
+
+
+if __name__=="__main__":
+	main()
