db7fe2618dfcbef910800f495eb102e787ca1a7a
cmput274 <cmput274@ualberta.ca>
2018-11-25 19:25:00 -0700

    working a* - needs cleaning!

--- python/maze.py
+++ python/maze.py
@@ -11,7 +11,7 @@ class node:
 		self.col = col # x ordinate
 		self.open = open # boolean - open or wall?
 		self.spot = (row, col)
-		self.visited = None
+		self.parent = None
 		self.chosen = None
 		# left right up down
 		self.neighbours = [(self.row, self.col+1),(self.row, self.col-1),(self.row+1, self.col),(self.row-1, self.col)]
@@ -57,6 +57,7 @@ def findPath(start, end, nodes, height, width):
 		neighbours = nodes[current].neighbours
 		for i in neighbours:
 			if not(i[0] < 0 or i[1] < 0 or i[0] > height or i[1] > width or i in closed or not nodes[i].open) and i not in open:
+				nodes[i].parent = current
 				open[i] = getCost(start, end, nodes[i].spot)
 
 
@@ -65,24 +66,24 @@ def distance(a, b):
 	return(trueIf)
 
 
-def finishPath(steps, end):
+def finishPath(steps, end, start, nodes):
 	path = []
 	flag = True
 	count = 0
+	x = end[0]
 	while(flag):
-		count = 0
-		for i in range(len(steps)-1):
-			if(distance(steps[i], steps[i+1]) == 1):
-				path.append(steps[i+1])
-			else:
-				count += 1
-		if count == 0:
-			flag = False
-		return(path[::-1]+end)
+		# if(distance(steps[i], steps[i+1]) == 1):
+		# 	path.append(steps[i+1]))
+		path.append(nodes[x].parent)
+		x = nodes[x].parent
+		if x == start[0]:
+			return(path[::-1]+end)
+
+	
 
 
 def main():
-    im = Image.open("/home/cmput274/ARCMPUT/arduino/final/274final/python/mazes/maze1_11X11.gif")
+    im = Image.open("/home/cmput274/ARCMPUT/arduino/final/274final/python/mazes/maze2_11X11.gif")
     mazeArr = np.array(im)
     print(mazeArr)
     width = im.size[0]
@@ -90,20 +91,22 @@ def main():
     start = [(0, i) for i in range(width) if im.getpixel((i, 0)) == 0]
     end = [(height-1, i) for i in range(width) if im.getpixel((i, height-1)) == 0]
     nodes = compileNodes(height, width, mazeArr, start[0], end[0])
-    c=finishPath(findPath(start[0], end[0], nodes, height, width)[::-1], end)
+    print(end, start)
+    # v = findPath(start[0], end[0], nodes, height, width)[::-1]
+    # for i in v:
+    # 	mazeArr[i[0]][i[1]] = 255
+
+    # print(mazeArr)   
+    c = finishPath(findPath(start[0], end[0], nodes, height, width)[::-1], end, start, nodes)
     print(c)
-    # for i in :
-    # 	mazeArr[c[0]][c[1]] = 9
+    for i in c:
+    	mazeArr[i[0]][i[1]] = 255
 
     print(mazeArr)
    # out = Image.new(RGB, (width*height), color = 0)
 
-    draw = ImageDraw.Draw(im)
-    for i in range(len(c)-1):
-    	draw.line(c[i]+c[i+1], fill = 128)
-
-    del draw
-    im.save( "out.PNG")
+    out = Image.fromarray(mazeArr)
+    out.save( "out.PNG")
 
 
 if __name__=="__main__":
