02993fd3b785663224c5e43a3289a40c11fafd64
konradStanski <kstanisz@ualberta.ca>
2018-11-28 00:38:56 -0700

    working accel stepper

--- arduino/comms.h
+++ arduino/comms.h
@@ -1,68 +1,54 @@
-
-
-// EXAMPLE TRANSMITTER
-/*
-#include <VirtualWire.h>
-
-void setup()
-{
-    Serial.begin(9600);   // Debugging only
-    Serial.println("setup");
-
-    // Initialise the IO and ISR
-    vw_set_ptt_inverted(true); // Required for DR3100
-    vw_setup(2000);  // Bits per sec
-}
-
-void loop()
-{
-    const char *msg = "hello";
-
-    digitalWrite(13, true); // Flash a light to show transmitting
-    vw_send((uint8_t *)msg, strlen(msg));
-    vw_wait_tx(); // Wait until the whole message is gone
-    digitalWrite(13, false);
-    delay(200);
-}
-*/
-
-// EXAMPLE RECEIVER CODE
-/*
-#include <VirtualWire.h>
-
-void setup()
-{
-    Serial.begin(9600); // Debugging only
-    Serial.println("setup");
-
-    // Initialise the IO and ISR
-    vw_set_ptt_inverted(true); // Required for DR3100
-    vw_setup(2000);  // Bits per sec
-
-    vw_rx_start();       // Start the receiver PLL running
-}
-
-void loop()
-{
-    uint8_t buf[VW_MAX_MESSAGE_LEN];
-    uint8_t buflen = VW_MAX_MESSAGE_LEN;
-
-    if (vw_get_message(buf, &buflen)) // Non-blocking
-    {
-    int i;
-
-        digitalWrite(13, true); // Flash a light to show received good message
-    // Message with a good checksum received, dump it.
-    Serial.print("Got: ");
-
-    for (i = 0; i < buflen; i++)
-    {
-        Serial.print(buf[i], HEX);
-        Serial.print(" ");
-    }
-    Serial.println("");
-        digitalWrite(13, false);
-    }
-}
+/*Wiring:
+gnd ble to gnd ard
+gnd ble to 5v ard
+wire rx ble to tx3 ard
+wire tx ble to rx3 ard
+led on pin 13
 
 */
+#include<Arduino.h>
+const int ledpin = 13;
+char val;
+
+void setup(){
+    init();
+    Serial.begin(9600);
+    Serial3.begin(9600);
+    pinMode(ledpin,OUTPUT);
+    //TEST OUTPUT LED
+    digitalWrite(ledpin, HIGH);
+    delay(300);
+    digitalWrite(ledpin, LOW);}
+
+int main() {
+    setup();
+
+    while (true){
+
+        // Receive from phone
+        if(Serial3.available()) {
+            //Serial.print("Received");
+            val = Serial3.read();
+            Serial.print(val);
+
+            // INDICATOR
+            digitalWrite(ledpin, HIGH);
+            //delay(200);
+            digitalWrite(ledpin,LOW);
+        }
+        // Transmit to phone
+        if(Serial.available()) {
+            //Serial3.print("Received")
+            val = Serial.read();
+            Serial3.print(val);
+
+            //INDICATOR
+            digitalWrite(ledpin, HIGH);
+            //delay(200);
+            digitalWrite(ledpin,LOW);
+        }
+
+    }
+
+    return 0;
+}
